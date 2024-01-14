2d2bb63ceda926d8f9f1e1f6e5a4f18f738444aa
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2024-01-14 15:38:40 -0800

    Write classes to open and close modal window

--- starter/script.js
+++ starter/script.js
@@ -5,5 +5,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Loop through the buttons and display the modal if clicked.
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+// Close the modal if the user clicked on the x
+btnCloseModal.addEventListener('click', closeModal);
+
+// Close the modal if the user clicked on the overlay
+overlay.addEventListener('click', closeModal);
